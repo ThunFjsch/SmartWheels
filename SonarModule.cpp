f000f178b2d4ae9ce6fe8e2097799684e0fab974
DrTowfiq-666 <122101575+DrTowfiq-666@users.noreply.github.com>
2023-10-19 11:52:49 +0200

    Update SonarModule.cpp

--- SonarModule.cpp
+++ SonarModule.cpp
@@ -1,43 +1,34 @@
 #include "SonarModule.h"
 #include <Arduino.h>
 
-int trigPinBack, echoPinBack, trigPinCenter, echoPinCenter, trigPinLeft, echoPinLeft, trigPinRight, echoPinRight;
+int trigPin echoPinBack, echoPinCenter, echoPinLeft, echoPinRight;
 float backDistance = 0;
 float frontDistances[2];
 
-void initSonarModule(int _trigPinBack, int _echoPinBack, int _trigPinCenter, int _echoPinCenter, int _trigPinLeft, int _echoPinLeft, int _trigPinRight, int _echoPinRight){
- 
-  trigPinBack = _trigPinBack;
+void initSonarModule(int _trigPin, int _echoPinBack, int _echoPinCenter, int _echoPinLeft, int _echoPinRight){
+  // Sets the given PinLayout to global variables
+
+  trigPin = _trigPin;
   echoPinBack = _echoPinBack;
-  trigPinCenter = _trigPinCenter;
   echoPinCenter = _echoPinCenter; 
-  trigPinLeft = _trigPinLeft;
   echoPinLeft = _echoPinLeft; 
-  trigPinRight = _trigPinRight;
   echoPinRight = _echoPinRight;
 
-  pinMode(trigPinBack, OUTPUT);
+ //Setup Sonar Pins
+  pinMode(trigPin, OUTPUT);
   pinMode(echoPinBack, INPUT);  
-  pinMode(trigPinCenter, OUTPUT);
   pinMode(echoPinCenter, INPUT);
-  pinMode(trigPinLeft, OUTPUT);
   pinMode(echoPinLeft, INPUT);  
-  pinMode(trigPinRight, OUTPUT);
   pinMode(echoPinRight, INPUT);
 }
-
+//Function gets updated readings of distance  
 void updatedSonarDistance(){
   // updating sonar hardware
-  digitalWrite(trigPinCenter, HIGH);
-  digitalWrite(trigPinLeft, HIGH);
-  digitalWrite(trigPinRight, HIGH);
-  digitalWrite(trigPinBack, HIGH);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(2);
-  digitalWrite(trigPinCenter, LOW);
-  digitalWrite(trigPinLeft, LOW);
-  digitalWrite(trigPinRight, LOW);
-  digitalWrite(trigPinBack, LOW);
+  digitalWrite(trigPin, LOW);
 
+ //calculation to convert the reading from the sonar sensor into centimeter. 
   float distanceCalc =  0.034 / 2; //distance in cm
   
   frontDistances[0] = pulseIn(echoPinLeft, HIGH) * distanceCalc;
@@ -46,20 +37,26 @@ void updatedSonarDistance(){
   backDistance = pulseIn(echoPinBack, HIGH) * distanceCalc;
   
 }
+//Function returns updated front reading in cm. 
 
 float GetAllFrontDistance(){
   return frontDistances[2];
 }
+//Function returns updataed back raeding in cm.
 
 float GetBackDistance(){
   return backDistance;
 }
+//Function to test accuracy of sonar sensors
+
 void TestingDistanceAndAccuracy(){
   Serial.println(frontDistances[0]);
   Serial.println(frontDistances[1]);
   Serial.println(frontDistances[2]);
   Serial.println(backDistance);
 }
+//Function to initiate test
+
 void sonarDebug(){
    TestingDistanceAndAccuracy();
 }
