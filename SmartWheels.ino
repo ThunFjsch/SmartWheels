ccf57f33e6e8d189909919c65ef17789681e6340
DrTowfiq-666 <122101575+DrTowfiq-666@users.noreply.github.com>
2023-10-19 12:06:15 +0200

    Update SmartWheels.ino

--- SmartWheels.ino
+++ SmartWheels.ino
@@ -3,18 +3,15 @@
 float BackDistance = 0;
 float FrontDistances[2];
 
-#define trigPinBack 0
+#define trigPin 0
 #define echoPinBack 1
-#define trigPinCenter 2
 #define echoPinCenter 3
-#define trigPinLeft 4
 #define echoPinLeft 5
-#define trigPinRight 6
 #define echoPinRight 7
 bool bibupDebug = true;
 void setup() {
   Serial.begin(9600);
-  initSonarModule(trigPinBack, echoPinBack, trigPinCenter, echoPinCenter, trigPinLeft, echoPinLeft, trigPinRight, echoPinRight);
+  initSonarModule(trigPin, echoPinBack, echoPinCenter, echoPinLeft, echoPinRight);
 }
 
 void loop() {
