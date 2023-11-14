7c5b16608438a5dbccaa3a2101d7461892d260f2
ThunFjsch <arinartz4@gmail.com>
2023-11-14 12:12:18 +0100

    Project changed to fit the Microchip Studio IDE

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ * SmartWheels.c
+ *
+ * Created: 14/11/2023 11:39:10
+ * Author : ThunFisch
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
