6cdb0b052eb871698450adba36014faa7a63956c
Osalem123 <144733718+Osalem123@users.noreply.github.com>
2023-12-15 20:28:15 +0100

    Add files via upload
    H file Ir sensor

---
+++ main H file.c
@@ -0,0 +1,28 @@
+/*
+ * GccApplication5.c
+ *
+ * Created: 15/12/2023 20:25:58
+ * Author : omar.salem
+ */ 
+
+// irsensor.h
+
+#ifndef IRSENSOR_H
+#define IRSENSOR_H
+
+#include <avr/io.h>
+
+#define LEFT_IR_PIN PD6
+#define RIGHT_IR_PIN PD7
+
+extern const int threshold;
+extern uint8_t leftIRState;
+extern uint8_t rightIRState;
+
+void initIRModule(uint8_t leftPin, uint8_t rightPin);
+uint16_t adc_read(uint8_t adc_channel);
+void updatedIRDetection();
+
+#endif 
+
+
