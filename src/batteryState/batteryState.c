b33e98dfe85523b2faac2b13b6e5c32c5f06dd6d
Benedictus Leo <leoaryasatya@gmail.com>
2024-01-22 14:59:39 +0100

    Fixed battery calculations

--- src/batteryState/batteryState.c
+++ src/batteryState/batteryState.c
@@ -19,7 +19,7 @@ uint16_t batteryADCRead(uint8_t channel) {
 	return ADC;
 }
 
-int map(int x, int in_min, int in_max, int out_min, int out_max) {
+int map(float x, float in_min, float in_max, int out_min, int out_max) {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
--- src/batteryState/batteryState.h
+++ src/batteryState/batteryState.h
@@ -6,7 +6,7 @@ extern int mappedVoltage;
 
 void batteryADCInit();
 uint16_t batteryADCRead(uint8_t channel);
-int map(int x, int in_min, int in_max, int out_min, int out_max);
+int map(float x, float in_min, float in_max, int out_min, int out_max);
 void batteryADC();
 
 #endif
